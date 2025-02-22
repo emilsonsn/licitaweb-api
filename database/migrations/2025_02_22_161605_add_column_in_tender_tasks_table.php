835dd3804aec58e45dc620eca4b8a9cb36c7e137
jonathassantana <jonathas.santanacg@gmail.com>
2025-02-22 16:31:13 -0300

    f

--- database/migrations/2025_02_22_161605_add_column_in_tender_tasks_table.php
+++ database/migrations/2025_02_22_161605_add_column_in_tender_tasks_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('tender_tasks', function (Blueprint $table) {
-            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('client_id')->nullable();
 
             $table->foreign('client_id')
                 ->references('id')
